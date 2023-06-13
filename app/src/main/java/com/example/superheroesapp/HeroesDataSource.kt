c027b3f0f3df48d2148157ce453cbafad7b15732
TranDatk <hgooshvhd123@gmail.com>
2023-06-13 16:57:43 +0700

    13/06/2023

--- app/src/main/java/com/example/superheroesapp/HeroesDataSource.kt
+++ app/src/main/java/com/example/superheroesapp/HeroesDataSource.kt
@@ -3,16 +3,18 @@ package com.example.superheroesapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.superheroesapp.ui.theme.SuperheroesAppTheme
 
-class MainActivity : ComponentActivity() {
+class HeroesDataSource : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
